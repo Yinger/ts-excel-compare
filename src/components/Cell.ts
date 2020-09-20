0374d4a08d74cec0de119aeee6139c70a5632110
林 <melody.crazycoding@gmail.com>
2020-09-20 15:51:29 +0800

    fix diff result

--- src/components/Cell.ts
+++ src/components/Cell.ts
@@ -1,36 +1,21 @@
-// import { isString } from "../utils/helper";
-
 class Cell {
   value: string;
+  instance: any;
 
-  constructor(value: string) {
+  constructor(value: string, instance: any) {
     this.value = value;
+    this.instance = instance;
   }
 
-  getCell() {
-    if (this.value !== null) return "" + this.value;
-    else return null;
+  getCell(x: number, y: number) {
+    let v = this.instance.getDataAtCell(y, x);
+    if (v == null) return v;
+    return "" + v;
   }
 
   getValue() {
     return this.value;
   }
-  //   setValue(newValue: string) {
-  //     console.log("Cell.setValue", { old: this.value, new: newValue });
-  //     this.value = newValue;
-  //   }
 }
-
-// export const getCellValue = (cell: any) => {
-//   let cellValue = "";
-//   if (cell) {
-//     if (typeof cell.getValue === "function") {
-//       cellValue = cell.getValue();
-//     } else if (isString(cell)) {
-//       cellValue = cell;
-//     }
-//   }
-//   return cellValue;
-// };
 
 export default Cell;
--- src/utils/RenderDiffTable.tsx
+++ src/utils/RenderDiffTable.tsx
@@ -11,19 +11,11 @@ export function renderDiff(
   value: Handsontable.CellValue,
   cellProperties: Handsontable.CellProperties
 ) {
-  //   var tt = {};
-  //   tt.getCell = function (x, y) {
-  //     let v = instance.getDataAtCell(y, x);
-  //     if (v == null) return v;
-  //     return "" + v;
-  //   };
-  var tt = new Cell(value);
+  var tt = new Cell(value, instance);
   var view = new daff.coopy.SimpleView();
   var cell = daff.coopy.DiffRender.renderCell(tt, view, col, row);
   var className = cell.category;
   var value2 = cell.pretty_value;
-
-  //   Handsontable.renderers.TextRenderer.apply(this, arguments);
   if (className !== "") {
     td.className = className;
   }
@@ -31,51 +23,3 @@ export function renderDiff(
   td.innerHTML = value2;
   return value2;
 }
-
-// const tt ={
-//     getCell:function;
-// };
-
-// class tt {
-
-//   getCell = (cell: any) => {
-//     let cellValue = null;
-//     if (cell) {
-//       if (typeof cell.getValue === "function") {
-//         cellValue = cell.getValue();
-//       } else if (isString(cell)) {
-//         cellValue = cell;
-//       }
-//     }
-
-//     if (cellValue !== null) cellValue = "" + cellValue;
-
-//     return cellValue;
-//   };
-// }
-
-// interface tt{
-//     getCell: any ;
-// }
-// tt.getCell = ()=>{
-
-// }
-
-// const isString = (value: any) => {
-//   return typeof value === "string" || value instanceof String;
-// };
-
-// const getCellValue = (cell: any) => {
-//   let cellValue = null;
-//   if (cell) {
-//     if (typeof cell.getValue === "function") {
-//       cellValue = cell.getValue();
-//     } else if (isString(cell)) {
-//       cellValue = cell;
-//     }
-//   }
-
-//   if (cellValue !== null) cellValue = "" + cellValue;
-
-//   return cellValue;
-// };
