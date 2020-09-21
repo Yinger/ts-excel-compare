2e26a979119854611262125b6028fa2ae3c847c8
Yinger <melody.crazycoding@gmail.com>
2020-09-21 10:26:25 +0900

    rebuild

---
+++ build/precache-manifest.cde56a77895f6586dacfe6320ea7c2b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a5a3b32018bdd1876346a49b1c6cf1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82707648a2ef9d9b4aca",
+    "url": "./static/css/2.c90114df.chunk.css"
+  },
+  {
+    "revision": "25fc02b642e6196e92ad",
+    "url": "./static/css/main.646e2a3d.chunk.css"
+  },
+  {
+    "revision": "82707648a2ef9d9b4aca",
+    "url": "./static/js/2.7b58d770.chunk.js"
+  },
+  {
+    "revision": "9a2dcca929021b156b9f93e1407c8091",
+    "url": "./static/js/2.7b58d770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fc02b642e6196e92ad",
+    "url": "./static/js/main.9ae0d7e2.chunk.js"
+  },
+  {
+    "revision": "d18b993f2a0a4f86eb89",
+    "url": "./static/js/runtime-main.3022dc8b.js"
+  },
+  {
+    "revision": "7bb6d51e9e37221c82c3767f16fb4e19",
+    "url": "./static/media/Find-VS2017.7bb6d51e.cs"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3798fd0d88efb93a9a422228a0c6b3a.js"
+  "./precache-manifest.cde56a77895f6586dacfe6320ea7c2b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
